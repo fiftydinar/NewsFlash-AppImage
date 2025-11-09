f5d1ef3922ec036f5282c6b28ca1917b7191e9a4
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-11-09 10:31:04 +0100

    Add fix for `libclapper`

--- newsflash-appimage.sh
+++ newsflash-appimage.sh
@@ -15,6 +15,17 @@ export DEPLOY_OPENGL=1
 export DEPLOY_GSTREAMER=1
 export STARTUPWMCLASS=newsflash # For Wayland, this is 'io.gitlab.news_flash.NewsFlash', so this needs to be changed in desktop file manually by the user in that case until some potential automatic fix exists for this
 
+## This app uses libclapper for video playback, so this is needed
+sys_clapper_dir=$(echo /usr/lib/clapper-*)
+if [ -d "$sys_clapper_dir" ]; then
+	export PATH_MAPPING="
+		$sys_clapper_dir:\${SHARUN_DIR}/lib/${sys_clapper_dir##*/}
+	"
+else
+	>&2 echo "ERROR: Cannot find the clapper lib dir"
+	exit 1
+fi
+
 # Trace and deploy all files and directories needed for the application (including binaries, libraries and others)
 wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
 chmod +x ./quick-sharun
